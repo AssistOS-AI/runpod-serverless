b6b487b2faae0655c4b47735edd173bc6feef1f5
Ilisei Daria-Stefania <125896202+iliseidaria@users.noreply.github.com>
2024-09-13 19:38:32 +0300

    Update handler.py

--- models/dentro_faceswap/handler.py
+++ models/dentro_faceswap/handler.py
@@ -45,8 +45,8 @@ def handler(job):
     # Initialize FaceAnalysis and the swapper model
     app = FaceAnalysis(name='buffalo_l')
     app.prepare(ctx_id=0, det_size=(640, 640))
-    model_path = '/app/inswapper_128.onnx'  # Adjust path as needed
-    swapper = get_model(model_path, download=False)  # Use local model if available
+    model_path = '/app/inswapper_128.onnx'  # Path where the model is downloaded
+    swapper = get_model(model_path, download=False)  # Use local model
 
     # Prepare the images
     def get_faces(image_np):
