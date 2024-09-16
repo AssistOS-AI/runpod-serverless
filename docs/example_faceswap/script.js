a68d872a7f2fe0c5aab43f2b9332a66cffd67c74
Ilisei Daria-Stefania <125896202+iliseidaria@users.noreply.github.com>
2024-09-16 13:41:02 +0300

    Update script.js

--- docs/example_faceswap/script.js
+++ docs/example_faceswap/script.js
@@ -141,7 +141,7 @@ function submitForm(event) {
         }
     };
 
-    fetch('https://api.runpod.ai/v2/v5qpjr27v87s0c/run', {
+    fetch('https://api.runpod.ai/v2/0bblhriy2mcg71/run', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -163,7 +163,7 @@ function submitForm(event) {
 }
 
 function checkStatus(requestId, apiKey) {
-    const statusUrl = `https://api.runpod.ai/v2/v5qpjr27v87s0c/status/${requestId}`;
+    const statusUrl = `https://api.runpod.ai/v2/0bblhriy2mcg71/status/${requestId}`;
     const loadingSpinner = document.getElementById('loadingSpinner');
     const form = document.getElementById('inputForm');
 
